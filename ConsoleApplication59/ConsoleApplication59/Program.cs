d148caf7ebaf1b4f6e50ca60471d750f35af6083
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-07 18:58:40 +0100

    Add files via upload

---
+++ ConsoleApplication59/ConsoleApplication59/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    sealed class Osoba
+    {
+
+    }
+
+    class Student:Osoba
+    {
+
+    }
+}
---
+++ ConsoleApplication60/ConsoleApplication60/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ccc c1 = new Ccc();
+            c1.Metoda();
+            Console.ReadKey();
+        }
+    }
+
+    class Aaa
+    {
+        public virtual void Metoda()
+        {
+            Console.WriteLine("aaa");
+        }
+    }
+
+    class Bbb:Aaa
+    {
+        public override sealed void Metoda()
+        {
+            Console.WriteLine("Bbb");
+        }
+    }
+
+    class Ccc:Bbb
+    {
+        //public override void Metoda()
+        //{
+
+        //}
+    }
+}
